fef116a7e1503c857937a368fba372433c1cee76
BerylBucket <BerylBucket@users.noreply.github.com>
2020-10-19 03:35:43 +0000

    Prettified Code!

--- src/Home.vue
+++ src/Home.vue
@@ -1,22 +1,22 @@
 <template>
-<div>
-  <Carousel></Carousel>
-  <section id="about">
-    <div class="aboutImg1"></div>
-    <div class="aboutCard">
-      <h2>About our furniture</h2>
-      <p>
-        Our multifunctional collection blends design and function to suit your
-        individual taste. Make each room unique, or pick a cohesive theme that
-        best express your interests and what inspires you. Find the furniture
-        pieces you need, from traditional to contemporary styles or anything in
-        between. Product specialists are available to help you create your dream
-        space.
-      </p>
-    </div>
-    <div class="aboutImg2"></div>
-  </section>
-</div>
+  <div>
+    <Carousel></Carousel>
+    <section id="about">
+      <div class="aboutImg1"></div>
+      <div class="aboutCard">
+        <h2>About our furniture</h2>
+        <p>
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme that
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </p>
+      </div>
+      <div class="aboutImg2"></div>
+    </section>
+  </div>
 </template>
 
 <script>
