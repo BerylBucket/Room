63d523b0262c6aa96a0933317cbf85015dd8ef16
BerylBucket <BerylBucket@users.noreply.github.com>
2020-10-19 02:01:03 +0000

    Prettified Code!

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,35 @@
 <template>
-<div class="navbar">
-  <img class="logo" src="https://raw.githubusercontent.com/BerylBucket/Room/main/assets/logo.svg" />
-  <a class="burger" @click="active = true"><img src="https://raw.githubusercontent.com/BerylBucket/Room/main/assets/icons/burger.svg" /></a>
-  <div class="links" :class="{active: active}">
-    <a class="close" @click="active = false"><img src="https://raw.githubusercontent.com/BerylBucket/Room/main/assets/icons/icon-close.svg" alt="close" /></a>
-    <a>home</a>
-    <a>shop</a>
-    <a>about</a>
-    <a>contact</a>
+  <div class="navbar">
+    <img
+      class="logo"
+      src="https://raw.githubusercontent.com/BerylBucket/Room/main/assets/logo.svg"
+    />
+    <a class="burger" @click="active = true"
+      ><img
+        src="https://raw.githubusercontent.com/BerylBucket/Room/main/assets/icons/burger.svg"
+    /></a>
+    <div class="links" :class="{ active: active }">
+      <a class="close" @click="active = false"
+        ><img
+          src="https://raw.githubusercontent.com/BerylBucket/Room/main/assets/icons/icon-close.svg"
+          alt="close"
+      /></a>
+      <a>home</a>
+      <a>shop</a>
+      <a>about</a>
+      <a>contact</a>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      active: false
+      active: false,
     };
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
@@ -33,7 +43,7 @@ export default {
   }
   .navbar .links {
     display: none;
-    }
+  }
   .links.active {
     display: initial;
     background: #f5f5f5;
@@ -57,7 +67,7 @@ export default {
   }
 }
 @media (min-width: 600px) and (max-width: 1023px) {
- .navbar .logo {
+  .navbar .logo {
     display: block;
     margin: auto;
   }
@@ -81,46 +91,47 @@ export default {
   }
   .links .close {
     margin-right: 20px;
-   display: inline-block;
+    display: inline-block;
   }
   .links a {
     display: inline-block;
     margin-left: 20px;
     color: #000;
   }
-  .links a:hover{
+  .links a:hover {
     position: relative;
   }
-  .links a:hover:after{
+  .links a:hover:after {
     position: absolute;
     width: 20px;
     height: 2px;
     background: #000;
     content: "";
     left: calc(50% - 10px);
-    bottom:-5px;
+    bottom: -5px;
   }
-.links .close:hover:after{
-  display: none;
-}
-}
-@media (min-width: 1024px){
-  .burger, .close{
+  .links .close:hover:after {
     display: none;
   }
-  .navbar .logo{
+}
+@media (min-width: 1024px) {
+  .burger,
+  .close {
+    display: none;
+  }
+  .navbar .logo {
     margin-right: 20px;
   }
-  .navbar .links{
+  .navbar .links {
     display: inline;
   }
-  .links a{
+  .links a {
     margin-left: 26px;
   }
-  .links a:hover{
+  .links a:hover {
     position: relative;
   }
-  .links a:hover:after{
+  .links a:hover:after {
     position: absolute;
     width: 20px;
     height: 2px;
@@ -129,8 +140,8 @@ export default {
     left: calc(50% - 10px);
     bottom: -5px;
   }
-.links .close:hover:after{
-  display: none;
-}
+  .links .close:hover:after {
+    display: none;
+  }
 }
 </style>
